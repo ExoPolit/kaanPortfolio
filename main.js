99ff7704e7582cce23ce9ab198157c72a0dec793
ExoPolit <exopolit@gmx.de>
2023-09-17 15:57:12 +0100

    new update

--- main.js
+++ main.js
@@ -3,23 +3,34 @@
 // wGSkAvoxM_J34F_B9
 //
 
-/*var slideIndex = 1;
-showDivs(slideIndex);
+// SLIDESHOW
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";
-} */
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
 let isModalOpen = false;
 let contrastToggle = false;
